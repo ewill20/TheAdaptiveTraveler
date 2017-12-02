78c24025aa5007ed1a94dec15399140839c04b1a
Eric Willard <ewill2020@gmail.com>
2017-12-02 13:25:14 -0600

    working on database functionality between local and heroku

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 5000;
 
 // Serving up static assets //
 app.use(express.static("public"));
-app.use(express.static('MDB'));
+
 
 // Standard code for body-parser //
 app.use(bodyParser.json());
