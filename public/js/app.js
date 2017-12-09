b00b84f5b41dccb87b1cbfefbb32c30502692af1
Eric Willard <ewill2020@gmail.com>
2017-12-09 10:42:42 -0600

    commented out google maps init

--- public/js/app.js
+++ public/js/app.js
@@ -3,13 +3,13 @@ $(document).ready(function() {
 });
 
 
-function initMap() {
-	// Create a map object and specify the DOM element for display //
-	var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 52.52000, lng: 13.4050},
-          zoom: 8
-})
-};
+// function initMap() {
+// 	// Create a map object and specify the DOM element for display //
+// 	var map = new google.maps.Map(document.getElementById('map'), {
+//           center: {lat: 52.52000, lng: 13.4050},
+//           zoom: 8
+// })
+// };
 // "create profile" button click listener //
 // $("#create-profile").on("click", function() {
 // 	window.location = "/signup";
