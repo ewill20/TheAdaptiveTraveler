5c4cbf0b0cc8b9a33798693e2f0bb66a2160ecd0
ewill20 <ewill2020@gmail.com>
2019-10-29 13:46:29 -0500

    Revert "trying to get a clean working tree"
    This reverts commit 23077fbafc62851506bd713f638b2c1c5ef9ffb9.

--- server.js
+++ server.js
@@ -6,10 +6,8 @@ var passport = require('passport')
 var session = require('express-session')
 var bodyParser = require('body-parser')
 var env = require('dotenv').load()
-var customEnv = require('env')
 var exphbs = require('express-handlebars')
 const path = require('path')
-var fs = require('fs')
 const mysql = require('mysql')
 var db = require("./models")
 
@@ -24,7 +22,7 @@ app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 
 
-var PORT = app.listen(process.env.PORT) || 5000;
+var PORT = process.env.PORT || 5000;
 
 
 // Standard code for body-parser //
