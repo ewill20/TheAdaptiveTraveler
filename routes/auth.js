a1e0ed39a21c7db475538cd38688a8abe062b3c3
ewill20 <ewill2020@gmail.com>
2019-10-31 12:49:06 -0500

    trying to fix the app from crashing

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const passport = require ('../config/passport/passport.js');
 
 var authController = require('../controllers/authcontroller.js');
 
-module.exports = function(app,passport){
+module.exports = function(app,passport) {
 
 app.get('/landing', authController.landing);
 
@@ -27,19 +27,42 @@ app.get('/popDestinations', authController.popDestinations);
 
 app.post('/landing', function(req, res, next) {
     console.log("whatever");
-    passport.authenticate('local-signup');
-});
+        passport.authenticate('local-signup');
+        passport.authenticate('local-signup', function(err, user, info) {
+            if (err) {
+                return next(err); // will generate a 500 error
+            }
+            console.log("whatever 2");
+            console.log(email);
+            try {
+                res.render('/profile');
+            } catch (err) {
+                console.log(err)
+                res.redirect('/signup')
+            }
+        });
+    });
+
+    app.get('/profile', isLoggedIn, authController.profile);
 
 
-app.get('/profile',isLoggedIn, authController.profile);
- 
+    app.get('/logout', authController.logout);
 
-function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated())
-        return next();
 
-    res.redirect('/signin');
-}
+    app.post('/signin', passport.authenticate('local'),
+        function(req, res) {
+            res.redirect('/profile' + req.user.handle)
+        });
+
+
+
+
+    function isLoggedIn(req, res, next) {
+        if (req.isAuthenticated())
+            return next();
+
+        res.redirect('/signin');
+    }
 
 
 }
