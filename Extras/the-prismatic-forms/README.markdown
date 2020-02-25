88b7d2b8fc46b0568290709139bde7ef69dc88b8
ewill20 <ewill2020@gmail.com>
2020-02-24 22:13:40 -0600

    still working on design and functionality of signin/signup page

---
+++ Extras/the-prismatic-forms/README.markdown
@@ -0,0 +1,6 @@
+# The prismatic forms 
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/nourabusoud/pen/BxJbjJ](https://codepen.io/nourabusoud/pen/BxJbjJ).
+
+ A new idea to have all your forms in one place!
+
+P.s all forms lead to the Thank you page for demonstration only.
--- public/js/app.js
+++ public/js/app.js
@@ -141,6 +141,29 @@ $('#pop-destinations-aqua').on("click", function() {
 $('#send-button').on('click', function() {
 	window.location = '/profile';
 })
+let prism = document.querySelector(".rec-prism");
+
+function showSignup(){
+  prism.style.transform = "translateZ(-100px) rotateY( -90deg)";
+}
+function showLogin(){
+  prism.style.transform = "translateZ(-100px)";
+}
+function showForgotPassword(){
+  prism.style.transform = "translateZ(-100px) rotateY( -180deg)";
+}
+
+function showSubscribe(){
+  prism.style.transform = "translateZ(-100px) rotateX( -90deg)";
+}
+
+function showContactUs(){
+  prism.style.transform = "translateZ(-100px) rotateY( 90deg)";
+}
+
+function showThankYou(){
+  prism.style.transform = "translateZ(-100px) rotateX( 90deg)";
+}
 
 
 
